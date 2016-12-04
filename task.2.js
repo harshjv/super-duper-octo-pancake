805242a7e0f75f9c847d35389369343f078f2302
Boris <boris@crypti.me>
2016-12-04 05:46:39 +0300

    Updated tests

--- task.2.js
+++ task.2.js
@@ -1,5 +1,18 @@
-let calc = (a, b) => {
+/*
+	Calculating float and returns float
+ */
+let calcFloat = (a, b) => {
+  return a + b;
+}
+
+/*
+	Calculate big number and returns string
+ */
+let calcBig = (a, b) => {
   return (a + b).toString();
 }
 
-module.exports = calc;
+module.exports = {
+  calcFloat: calcFloat,
+  calcBig: calcBig
+};
--- test/test.2.js
+++ test/test.2.js
@@ -6,13 +6,13 @@ chai.should();
 
 describe("Calculations", () => {
   it.skip("Should return correct result from calculating two floats", () => {
-	 let result = calc(0.1, 0.2);
+	 let result = calc.calcFloat(0.1, 0.2);
 	 
-	 result.should.equal('0.3');
+	 result.should.equal(0.3);
   });
   
   it("Should return correct result to big numbers", () => {
-	 let result = calc(1000000000000000000, 10);
+	 let result = calc.calcBig(1000000000000000000, 10);
 	 
 	 result.should.equal('1000000000000000010');
   });
